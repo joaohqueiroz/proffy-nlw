96355b21ed09e6967808ff3d68f9c93a168db4f0
João Henrique <johnrick.jh@gmail.com>
2020-08-07 14:26:12 -0300

    Connecting backend and frontend applications

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,33 +2,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+        avatar: string,
+        bio: string,
+        cost: number,
+        id: number,
+        name: string,
+        subject: string
+        whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/37818528?s=460&u=01920f117e51ca48229cc17d341a7ad5438b6e2f&v=4" alt="João Henrique" />
+                <img src={teacher.avatar} alt="Avatar" />
                 <div>
-                    <strong>João Henrique</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Entusiasta das melhores tecnlogias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar vidas de pessoas através de experiências. Mais de 200.000 já passaram por uma das minhas explosões.
+                {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
